deda0cd9386c93be7def5eb2a2839a5d09df73a7
Tsai Li Ming <tsai.liming@gmail.com>
2020-06-20 08:53:59 +0800

    Update to actually clean up

--- ansible/clean-ccnrd.sh
+++ ansible/clean-ccnrd.sh
@@ -5,17 +5,12 @@ WORKLOAD=ocp4-workload-ccnrd
 USER_COUNT=5
 PASSWORD=r3dh4t1!
 
-oc delete knativeservings.operator.knative.dev knative-serving -n knative-serving
-oc delete namespace knative-serving
-oc delete knativeeventings.operator.knative.dev knative-eventing -n knative-eventing
-oc delete namespace knative-eventing
-oc get crd -oname | grep 'knative.dev' | xargs oc delete
-
-
 for ns in `oc get ns | grep user[0-9]-istio-system | awk '{print $1}'`;
 do
-    oc delete servicemeshcontrolplanes -n $ns scmp
+    oc delete -n $ns servicemeshcontrolplane.maistra.io/smcp 
+    sleep 1m
 done
+
 oc delete validatingwebhookconfiguration/openshift-operators.servicemesh-resources.maistra.io
 oc delete mutatingwebhoookconfigurations/openshift-operators.servicemesh-resources.maistra.io
 oc delete -n openshift-operators daemonset/istio-node
@@ -23,7 +18,13 @@ oc delete clusterrole/istio-admin clusterrole/istio-cni clusterrolebinding/istio
 oc get crds -o name | grep '.*\.istio\.io' | xargs oc delete
 oc get crds -o name | grep '.*\.maistra\.io' | xargs oc delete
 
-oc get subscriptions -n openshift-operators -o name | xargs o delete -n openshift-operators
+oc delete knativeservings.operator.knative.dev knative-serving -n knative-serving
+oc delete namespace knative-serving
+oc delete knativeeventings.operator.knative.dev knative-eventing -n knative-eventing
+oc delete namespace knative-eventing
+oc get crd -oname | grep 'knative.dev' | xargs oc delete
+
+oc get subscriptions -n openshift-operators -o name | xargs oc delete -n openshift-operators
 
 oc get csv -n openshift-operators -o name | xargs oc delete -n openshift-operators
 
