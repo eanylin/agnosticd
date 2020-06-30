245e4e9047a4321337a841921f38bdfd38ca9241
Tsai Li Ming <tsai.liming@gmail.com>
2020-06-30 17:01:50 +0800

    update

--- ansible/run-mlops.sh
+++ ansible/run-mlops.sh
@@ -4,19 +4,20 @@ OCP_USERNAME="opentlc-mgr"
 WORKLOAD="ocp4-workload-mlops"
 USER_COUNT=5
 PASSWORD=r3dh4t1!
-ACTION=create #remove
+ACTION=create # create #remove
 
-ansible-playbook -i localhost, -c local ./configs/ocp-workloads/ocp-workload.yml \
-    -e "ansible_python_interpreter=`which python`" \
-    -e"ACTION=create" \
-    -e"ocp_username=${OCP_USERNAME}" \
-    -e"ocp_workload=roles_ocp_workloads/ocp4_workload_authentication" \
-    -e"silent=False" \
-    -e"ocp4_workload_authentication_remove_kubeadmin=False" \
-    -e"ocp4_workload_authentication_htpasswd_user_count=${USER_COUNT}" \
-    -e"ocp4_workload_authentication_admin_user=${OCP_USERNAME}" \
-    -e"ocp4_workload_authentication_htpasswd_admin_password=${PASSWORD}" \
-    -e"ocp4_workload_authentication_htpasswd_user_password=${PASSWORD}"
+# This to reset password
+ ansible-playbook -i localhost, -c local ./configs/ocp-workloads/ocp-workload.yml \
+     -e "ansible_python_interpreter=`which python`" \
+     -e"ACTION=create" \
+     -e"ocp_username=${OCP_USERNAME}" \
+     -e"ocp_workload=roles_ocp_workloads/ocp4_workload_authentication" \
+     -e"silent=False" \
+     -e"ocp4_workload_authentication_remove_kubeadmin=False" \
+     -e"ocp4_workload_authentication_htpasswd_user_count=${USER_COUNT}" \
+     -e"ocp4_workload_authentication_admin_user=${OCP_USERNAME}" \
+     -e"ocp4_workload_authentication_htpasswd_admin_password=${PASSWORD}" \
+     -e"ocp4_workload_authentication_htpasswd_user_password=${PASSWORD}"
 
 #a TARGET_HOST is specified in the command line, without using an inventory file
 ansible-playbook -i localhost, -c local ./configs/ocp-workloads/ocp-workload.yml \
