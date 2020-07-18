9823d72377699002e98262b6991c16e70f2d8b89
Tsai Li Ming <tsai.liming@gmail.com>
2020-07-18 12:16:49 +0800

    Check for gogs filter to be ready

--- ansible/run-mlops.sh
+++ ansible/run-mlops.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 OCP_USERNAME="opentlc-mgr"
-WORKLOAD="ocp4-workload-mlops"
+WORKLOAD="ocp4-workload-mlops" #ocp4-workload-ceph 
 USER_COUNT=5
 PASSWORD=r3dh4t1!
-ACTION=create # create #remove
+ACTION=create #create # create #remove
 
 # This to reset password
- ansible-playbook -i localhost, -c local ./configs/ocp-workloads/ocp-workload.yml \
-     -e "ansible_python_interpreter=`which python`" \
-     -e"ACTION=create" \
-     -e"ocp_username=${OCP_USERNAME}" \
-     -e"ocp_workload=roles_ocp_workloads/ocp4_workload_authentication" \
-     -e"silent=False" \
-     -e"ocp4_workload_authentication_remove_kubeadmin=False" \
-     -e"ocp4_workload_authentication_htpasswd_user_count=${USER_COUNT}" \
-     -e"ocp4_workload_authentication_admin_user=${OCP_USERNAME}" \
-     -e"ocp4_workload_authentication_htpasswd_admin_password=${PASSWORD}" \
-     -e"ocp4_workload_authentication_htpasswd_user_password=${PASSWORD}"
+ansible-playbook -i localhost, -c local ./configs/ocp-workloads/ocp-workload.yml \
+    -e "ansible_python_interpreter=`which python`" \
+    -e"ACTION=create" \
+    -e"ocp_username=${OCP_USERNAME}" \
+    -e"ocp_workload=roles_ocp_workloads/ocp4_workload_authentication" \
+    -e"silent=False" \
+    -e"ocp4_workload_authentication_remove_kubeadmin=False" \
+    -e"ocp4_workload_authentication_htpasswd_user_count=${USER_COUNT}" \
+    -e"ocp4_workload_authentication_admin_user=${OCP_USERNAME}" \
+    -e"ocp4_workload_authentication_htpasswd_admin_password=${PASSWORD}" \
+    -e"ocp4_workload_authentication_htpasswd_user_password=${PASSWORD}"
 
-#a TARGET_HOST is specified in the command line, without using an inventory file
+#TARGET_HOST is specified in the command line, without using an inventory file
 ansible-playbook -i localhost, -c local ./configs/ocp-workloads/ocp-workload.yml \
     -e "ansible_python_interpreter=`which python`" \
     -e"ACTION=$ACTION" \
@@ -27,4 +27,3 @@ ansible-playbook -i localhost, -c local ./configs/ocp-workloads/ocp-workload.yml
     -e"ocp_workload=${WORKLOAD}" \
     -e"silent=False" \
     -e"num_users=${USER_COUNT}" 
-
